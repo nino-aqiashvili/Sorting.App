16295509bb1bec6f6abe37bf98a5834140a4d94c
nino.aqiashvili <nino.aqiashvili@gmail.com>
2024-05-04 18:28:22 +0400

    task

--- src/test/java/org/example/SortingTest.java
+++ src/test/java/org/example/SortingTest.java
@@ -11,7 +11,6 @@ public class SortingTest {
     public void testNullCase(){
         sorting.sort(null);
     }
-
     @Test
     public void testSingleElementArrayCase() {
         int[] array = new int[]{300};
